b867cc1a7a0dd9b73ed3c0be9f9045f45ae29495
Obrevko Yurii <smile.oym@gmail.com>
2020-07-27 12:03:56 +0300

    home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./scss/style.scss";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="home">
+      <div className="border-line"></div>
+      <section className="anceta">
+        <div className="user-name">
+          Denis
+          <br /> Novik
+        </div>
+        <div className="user-information">
+          UX | UI designer <br /> 24 years old, Minsk
+        </div>
+        <div className="language">RU | ENG </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
